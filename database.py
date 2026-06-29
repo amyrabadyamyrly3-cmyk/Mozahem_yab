95570fab74fe8c9e6d79a590dd0fcda20cd8e1a1
amyrabadyamyrly3-cmyk <amyrabadyamyrly3@gmail.com>
2026-06-29 19:51:44 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,24 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect("data.db", check_same_thread=False)
+conn = sqlite3.connect("bot.db", check_same_thread=False)
 cursor = conn.cursor()
 
+# 👤 کاربران
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
+    name TEXT,
     points INTEGER DEFAULT 0,
     referrals INTEGER DEFAULT 0,
     searches INTEGER DEFAULT 0,
-    name TEXT
+    vip INTEGER DEFAULT 0
 )
 """)
 
+# 🔍 لاگ‌ها
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS logs (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
     query TEXT,
-    type TEXT,
     time TEXT
 )
 """)
